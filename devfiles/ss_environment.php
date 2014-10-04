c2d0d16425c2d617afda33aae2b4803ced18ff0f
Anselm Christophersen <ac@anselm.dk>
2014-10-04 18:08:34 +0200

    added ss environment file

---
+++ devfiles/ss_environment.php
@@ -0,0 +1,11 @@
+<?php
+define('SS_DATABASE_SERVER', 'localhost');
+define('SS_DATABASE_USERNAME', 'root');
+define('SS_DATABASE_PASSWORD', 'dont-use-in-production');
+define('SS_DATABASE_NAME', 'todos');
+
+define('SS_ENVIRONMENT_TYPE', 'dev');
+
+define('SS_DEFAULT_ADMIN_USERNAME', 'info@title.dk');
+define('SS_DEFAULT_ADMIN_PASSWORD', 'password');
+
